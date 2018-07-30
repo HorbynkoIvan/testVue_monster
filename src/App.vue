0b5d4dfc6baa4d5630606746700bbaa3deee77fb
Ivan <horbynko.i@gmail.com>
2018-07-30 22:45:55 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <myUser
+      v-for="user in users"
+
+      v-bind:description="user.description"   v-bind:name="user.name"
+      v-bind:key="users.name"
+    />
+  </div>
+</template>
+
+<script>
+  import user from './User';
+
+  export default {
+    components: {
+      myUser: user
+    },
+    data() {
+      return {
+        users: [
+          {name: "Alex", description: "This is our admin"},
+          {name: "Fritz", description: "This is our manager"},
+          {name: "John", description: "This is our client"}
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/User.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>{{upperCaseName()}}</h3>
+    <p>{{description}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    /*props: ['name', 'description'], */
+    props: {
+      name: {
+        type: String,
+        default: 'Not set'
+      },
+      description: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      upperCaseName() {
+        return this.name.toUpperCase()
+      }
+    }
+  }
+</script>
